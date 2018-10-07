d396d6edffefdc9b288f879ce4152030e48e8e75
Unknown <topfloor93@naver.com>
2018-10-08 02:14:05 +0900

    except indent all clear

--- main.py
+++ main.py
@@ -6,21 +6,32 @@ def load_log():
     str = ""
     file = open("log_file.txt", "r")
     file.readline()
-    #logs = file.readlines()
-    #for line in logs:
-    #    str += line + ","
-    str += file.readline() + ","
-    str += file.readline() + ","
+    logs = file.readlines()
+    for line in logs:
+        str += line + ","
+    #str += file.readline() + ","
+    #str += file.readline() + ","
     file.close()
 
     return str.replace('\n', ' ').replace('\r', '')
 
 
+def make_new_log(log):
+    file = open("new_log.txt", "w")
+    file.write(log)
+    file.close()
+
+
 if __name__ == "__main__":
 
+    res = ""
     log = load_log()
     dicts = ast.literal_eval(log)
     feature = Feature()
     for dict in dicts:
         feature.update_feature(dict)
-        print(feature.__dict__)
+        res += str(feature.__dict__) + ", "
+    res = res[:len(res)-2]
+    make_new_log(res)
+
+
--- ml_feature.py
+++ ml_feature.py
@@ -1,29 +1,22 @@
 class Feature:
     def __init__(self):
-        self.duration = ""
+        self.duration = ""  #
         self.header_len = ""
-        self.arrival_time = ""
-        self.total_packet = ""
-        self.variance_data_byte = ""
+        self.arrival_time = ""  #
         self.dev_eui = ""
         self.app_eui = ""
         self.direction = ""
         self.proto = ""
-        self.flags = ""
-        self.ts = ""
-        self.nfq_v = ""
-        self.action = ""
+        self.action = ""    #
         self.lorawan_mac_hdr = ""
-        self.lorawan_mac_var = ""
         self.lorawan_frm_hdr = ""
-        self.lorawan_frm_var = ""
         self.payload = ""
         self.payload_len = ""
         self.pktlen = ""
-        self.alert = ""
         self.fport = ""
         self.mtype = ""
         self.dev_addr = ""
+        self.fctrl = ""
         self.fcnt = ""
         self.fopts = ""
         self.gwsnr = ""
@@ -35,24 +28,80 @@ class Feature:
 
     def update_feature(self, _dict):
         list = _dict.values()
-        phypayload = list[4]
-        self.decode_lorawan(phypayload)
-        self.dev_eui = list[0]
-        self.app_eui = list[1]
-        self.fport = list[2]
-        self.dev_addr = list[3]
-        self.payload = list[5]
-        self.payload_len = list[7]
-        self.gweui = list[8].get('gwEUI')
-        self.gwlati = list[8].get('Lati')
-        self.gwlong = list[8].get('Long')
-        self.gwalti = list[8].get('Alti')
-        self.gwrssi = list[8].get('Rssi')
-        self.gwsnr = list[8].get('Snr')
-        self.pktlen = len(list[4])*2
+        self.pktlen = len(_dict.get('PHYPayload'))/2
+        phypayload = self.decode_lorawan(_dict.get('PHYPayload'))
+        self.dev_eui = _dict.get('DevEUI')
+        self.app_eui = _dict.get('AppEUI')
+        self.fport = _dict.get('FPort')
+        self.dev_addr = _dict.get('DevAddr')
+        self.payload = _dict.get('content')
+        self.payload_len = _dict.get('contentSize')
+        self.gweui = parse_gwinfo(_dict.get('gwInfo')).get('gwEUI')
+        self.gwlati = parse_gwinfo(_dict.get('gwInfo')).get('Lati')
+        self.gwlong = parse_gwinfo(_dict.get('gwInfo')).get('Long')
+        self.gwalti = parse_gwinfo(_dict.get('gwInfo')).get('Alti')
+        self.gwrssi = parse_gwinfo(_dict.get('gwInfo')).get('Rssi')
+        self.gwsnr = parse_gwinfo(_dict.get('gwInfo')).get('Snr')
         self.header_len = self.pktlen - self.payload_len
+        self.proto = 'lorawan'
+        self.lorawan_frm_hdr = phypayload['FHDR']
+        self.lorawan_mac_hdr = phypayload['MHDR']
+        self.mtype = phypayload['MType']
+
+        if self.mtype == '010' or self.mtype == '100':
+            self.direction = 'up'
+        else:
+            self.direction = 'down'
+
+        self.fctrl = phypayload['Fctrl']
+        self.fcnt = phypayload['FCnt']
+        self.fopts = phypayload['FOpts']
 
     def decode_lorawan(self, phypayload):
-        lorawan_dict = {}
+        macphyload = phypayload[2:int(self.pktlen)*2-8]
+        foptslen = get_foptslen(macphyload[8:10])
+        lorawan_dict = {'MHDR': phypayload[:2],
+                        'MACPayload': macphyload,
+                        'MIC': phypayload[int(self.pktlen)*2-8:int(self.pktlen)*2],
+                        'FHDR': macphyload[:14+foptslen],
+                        'Fport': macphyload[14+foptslen:16+foptslen],
+                        'FRMPayload': macphyload[16+foptslen],
+                        'DevAddr': self.dev_addr,
+                        'Fctrl': macphyload[8:10],
+                        'FCnt': macphyload[10:14],
+                        'FOpts': macphyload[14:14+foptslen],
+                        'MType': hex2bin(phypayload[:2])[:3]}
 
         return lorawan_dict
+
+
+def get_foptslen(fctrl):
+    return int(fctrl[1:], 16)
+
+
+def dec2bin(n):
+    bStr = ''
+    if n == 0: return '0'
+    while n > 0:
+        bStr = str(n % 2) + bStr
+        n = n >> 1
+    bStr = str(n % 2) + bStr
+    return bStr
+
+
+def hex2bin(hex):
+    return dec2bin(int(hex, 16))
+
+
+def parse_gwinfo(gwinfo):
+    res = {}
+    gwinfo = gwinfo[:len(gwinfo)-1]
+    gwinfo = gwinfo.replace(":", " ")
+    gwinfo = gwinfo.replace("  ", " ")
+    temp = gwinfo.split(" ")
+
+    for i in range(0, 12, 2):
+        res[temp[i]] = temp[i+1]
+
+    return res
+
